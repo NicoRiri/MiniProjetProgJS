0ead598203b2a6689e5ab828fcd08407b5b8fffd
ElsRiri <kiko.bernardet@gmail.com>
2023-02-14 20:57:42 +0100

    Fin de l'étape 3

--- js/noteListMenuView.js
+++ js/noteListMenuView.js
@@ -3,12 +3,16 @@ const noteListMenuView = {
         let selector= document.querySelector("#noteListMenu");
         selector.innerHTML = '';
         notes.forEach(note => this.displayItem(note));
+        for (let i = 0; i < selector.children.length; i++) {
+            selector.children.item(i).classList.remove("note_list_item-selected")
+        }
     },
 
     displayItem: function(note) {
         let element = document.querySelector("#noteListMenu");
         let div = document.createElement("div");
         div.classList.add("note_list_item");
+        div.classList.add("note_list_item-selected");
         let textNode = document.createTextNode(note.titre + " " + note.date_creation.toLocaleDateString());
         div.appendChild(textNode);
         div.addEventListener("click", function() {
@@ -24,14 +28,15 @@ spot.addEventListener("click", function (e) {
     console.log(e.target);
     let i = 0;
     for (const child of e.currentTarget.children) {
-        if (child === e.target) {
+        child.classList.remove("note_list_item-selected")
+        if (child == e.target) {
+            child.classList.add("note_list_item-selected")
             console.log("ok");
             console.log(i);
             application.indexNoteCourante = i;
             application.noteCourante = application.noteList.getNoteById(i);
             let nv = new Noteview(application.noteCourante);
             nv.afficher(nv.conversion())
-            break;
         } else {
             i++;
         }
