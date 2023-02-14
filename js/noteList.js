df71d01ef9c0f0b31768e10e74870feed509c92f
Theo.pz <100997816+pellizza1u@users.noreply.github.com>
2023-02-14 16:52:56 +0100

    Etape 3 finie

--- js/noteList.js
+++ js/noteList.js
@@ -1,15 +1,35 @@
-class NoteList{
+class NoteList {
     constructor() {
         this.tab = [];
+        this.load();
     }
-    addNote(note){
-        return this.tab.push(note);
+
+    addNote(note) {
+        let id = this.tab.push(note);
+        this.save();
+        return id - 1;
     }
-    getNoteById(id){
-        return this.tab.at(id);
+
+    getNoteById(id) {
+        return this.tab[id];
     }
-    getList(){
+
+    getList() {
         return this.tab;
     }
 
+    save() {
+        localStorage.setItem('noteList', JSON.stringify(this.tab));
+    }
+
+    load() {
+        let notes = localStorage.getItem('noteList');
+        if (notes) {
+            let value = JSON.parse(notes);
+            for (let note of value) {
+                this.addNote(new Note(note.titre, note.contenu, new Date(note.date_creation)));
+            }
+            noteListMenuView.initList(this.tab);
+        }
+    }
 }
--- js/noteListMenuView.js
+++ js/noteListMenuView.js
@@ -1,20 +1,30 @@
 const noteListMenuView = {
-    displayItem(note) {
-        let spot = document.querySelector("#noteListMenu");
+    initList: function(notes) {
+        let selector= document.querySelector("#noteListMenu");
+        selector.innerHTML = '';
+        notes.forEach(note => this.displayItem(note));
+    },
+
+    displayItem: function(note) {
+        let element = document.querySelector("#noteListMenu");
         let div = document.createElement("div");
         div.classList.add("note_list_item");
-        let text = document.createTextNode(note.titre + " " + note.date_creation.toLocaleDateString());
-        div.appendChild(text);
-        spot.appendChild(div);
-    },
-}
+        let textNode = document.createTextNode(note.titre + " " + note.date_creation.toLocaleDateString());
+        div.appendChild(textNode);
+        div.addEventListener("click", function() {
+            let noteView = new Noteview(note);
+            noteView.afficher(noteView.conversion())
+        });
+        element.appendChild(div);
+    }
+};
 
 let spot = document.querySelector("#noteListMenu");
 spot.addEventListener("click", function (e) {
     console.log(e.target);
     let i = 0;
     for (const child of e.currentTarget.children) {
-        if (child == e.target) {
+        if (child === e.target) {
             console.log("ok");
             console.log(i);
             application.indexNoteCourante = i;
@@ -26,12 +36,5 @@ spot.addEventListener("click", function (e) {
             i++;
         }
       }
-        /*
-        if (note.titre + " " + note.date_creation.toLocaleDateString() == e.target) {
-            console.log("ok");
-            application.noteCourante = note;
-            let nv = new Noteview(note);
-            nv.afficher(nv.conversion())
-        }
-        */
-})
+    }
+)
